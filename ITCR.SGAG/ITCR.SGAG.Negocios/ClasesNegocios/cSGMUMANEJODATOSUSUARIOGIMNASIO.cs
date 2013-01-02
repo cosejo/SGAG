4d3158efbc1b8e4216f4d4bb15fdd3a11e49bad6
Natalia Quiros <nataliaq15@gmail.com>
2013-01-02 16:03:05 -0600

    Proyecto de Pruebas

---
+++ ITCR.SGAG/ITCR.SGAG.Negocios/ClasesNegocios/cSGMUMANEJODATOSUSUARIOGIMNASIO.cs
@@ -0,0 +1,29 @@
+﻿#region Acerca de...
+///////////////////////////////////////////////////////////////////////////
+// Cliente:  Tecnológico de Costa Rica
+// Proyecto: ITCR.SGAG
+// Descripción: Clase de LOGICA DE NEGOCIOS para manejo de los datos de los estudiantes y funcionarios obtenidos del WSSeguridad
+// 
+// Fecha: miercoles, 1 de enero de 2013, 11:48:00 a.m.
+///////////////////////////////////////////////////////////////////////////
+#endregion
+
+
+using System;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using ITCR.SGAG.Base;
+using ITCR.SGAG.Datos;
+//using ITCR.SGAG.Negocios.wsSeguridad;
+
+namespace ITCR.SGAG.Negocios.ClasesNegocios
+{
+    /// <summary>
+    /// Propósito: Clase de lógica manejar los usuarios del gimnasio.
+    /// </summary>
+    public class cSGMUMANEJODATOSUSUARIOGIMNASIO
+    {
+
+    }
+}
---
+++ ITCR.SGAG/ITCR.SGAG.Pruebas/cSGPRUEBALOGIN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ITCR.SGAG.Pruebas
+{
+    [TestClass]
+    public class cSGPRUEBALOGIN
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
